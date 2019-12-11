9af77569418f3a0d72f2b4f66af1a692e05095aa
“Foster <fosterataylor@yahoo.com>
2019-12-11 09:44:39 -0700

    Setup initial ReservationForm state

---
+++ src/ReservationForm/ReservationForm.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class ReservationForm extends Component{
+    constructor() {
+        super();
+        this.state = {
+            id: '',
+            name: '',
+            date: '',
+            time: '',
+            number:''
+        }
+    }
+}
+
+export default ReservationForm
