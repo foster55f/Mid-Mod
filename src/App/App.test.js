b715d59476dd6f92f41c2203ef19fb99e941c33e
“Foster <fosterataylor@yahoo.com>
2019-12-11 11:01:02 -0700

    Add method test for App

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -8,4 +8,13 @@ describe('App', () => {
     const wrapper = shallow(<App />)
     expect(wrapper).toMatchSnapshot()
   })
+
+  it('should update the state with a new reservation when addReservation is called', () => {
+    const wrapper = shallow(<App />)
+    const mockReservation = { id: 55998882, name: 'foster', date: "2019-12-15", time: "5:00", number: "55" };
+    const expected=[mockReservation]
+    expect(wrapper.state('reservations')).toEqual();
+    wrapper.instance().addReservation(mockReservation)
+    expect(wrapper.state('reservations')).toEqual([expected]);
+  })
 })
