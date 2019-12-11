eb837214c078c451b3ae504b8f0bfebcf6119530
“Foster <fosterataylor@yahoo.com>
2019-12-11 09:22:27 -0700

    Add ReservationConatiner class structure

---
+++ src/ReservationContainer/ReservationContainer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ReservationContainer = () => {
+    // const allReservations = 
+    return (<ReservationCard
+        
+
+        />
+        
+    )
+
+    return (
+        <div>
+            {allReservations}
+        </div>
+    )
+}
+
+export default ReservationContainer
